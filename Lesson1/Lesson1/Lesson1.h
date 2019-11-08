db708a0630c31490edcbee4a10b27d4481ccaca8
Sergey Kharchenko <sergey.kharchenko@abbyy.com>
2019-11-08 13:11:44 +0300

    Initial commit

---
+++ Lesson1/Lesson1/Lesson1.h
@@ -0,0 +1,15 @@
+// Copyright (C) ABBYY (BIT Software), 1993-2019 . All rights reserved.
+// Автор: Sergey Kharchenko
+// Описание: @warning добавить описание
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lesson1.
+FOUNDATION_EXPORT double Lesson1VersionNumber;
+
+//! Project version string for Lesson1.
+FOUNDATION_EXPORT const unsigned char Lesson1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lesson1/PublicHeader.h>
+
+
